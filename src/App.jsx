a46dcf7671fedc00777f729895517934ae732451
lconnell1000 <lconnell1000@gmail.com>
2022-05-31 13:36:17 +0930

    filter method not workign for total results, need to try other method

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,27 @@ import Navbar from "./components/Navbar";
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
-  const [ totalResults, setTotalResults ]= useState("")
-  const [ radioValue, setRadioValue ]= useState('Any')
+  const [ totalResults, setTotalResults ]= useState("");
+  const [ radioValue, setRadioValue ]= useState('Any');
+  const [yearValue, setYearValue] = useState([1930, 2021]);
   
 
   const getMovieRequest = async (searchValue, radioValue) => {
     const typeString = (radioValue !== 'Any') ? "&type="+radioValue : "";
+    //for (y=yearValue[0]; y<=yearValue[1]; y++)
     const url =  `http://www.omdbapi.com/?s=${searchValue}${typeString}&apikey=dce48647`;
-   // console.log(url);
+    console.log(url);
     const response = await fetch(url);
+    
+    // filteredResponse = response.filter(response => response)
     const responseJson = await response.json();
-    console.log(responseJson);
+    const filteredResponse = responseJson.Search.filter(element => element.Year > yearValue[0] && element.Year < yearValue[1]);
+    console.log("filtered respone ", filteredResponse);
 
     if (responseJson.Search) {
-
-      setMovies(responseJson.Search)
-      setTotalResults(responseJson.totalResults)
+      
+      setMovies(filteredResponse);
+      setTotalResults(filteredResponse.length);
     }
     else {
       setMovies([])
@@ -32,12 +37,12 @@ const App = () => {
   };
 
   useEffect(() => {
-    getMovieRequest(searchValue, radioValue);
-  }, [searchValue, radioValue]);
+    getMovieRequest(searchValue, radioValue, yearValue);
+  }, [searchValue, radioValue, yearValue]);
 
   return (
   <div>
-    <Navbar searchValue={searchValue} setSearchValue={setSearchValue} radioValue={radioValue} setRadioValue={setRadioValue}/>
+    <Navbar searchValue={searchValue} setSearchValue={setSearchValue} radioValue={radioValue} setRadioValue={setRadioValue} yearValue={yearValue} setYearValue={setYearValue}/>
     <Movies totalResults={totalResults} setTotalResults={setTotalResults} movies={movies} className="column"/>
   </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import styled from "styled-components";
 import { Slider } from "@mui/material";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from '@mui/material/FormControl';
+import FormControl from "@mui/material/FormControl";
 import { laptop } from "../responsive";
 
 const Container = styled.div`
@@ -18,7 +17,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  ${laptop({ display: "block"})}
+  ${laptop({ display: "block" })}
 `;
 
 const Left = styled.div`
@@ -78,35 +77,35 @@ const RadioWrapper = styled.div`
 `;
 
 const Navbar = (props) => {
-  const [value, setValue] = useState([1950, 2021]);
-
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    props.setYearValue(newValue);
   };
 
   const handleRadioChange = (event, selectedValue) => {
-  props.setRadioValue(selectedValue);
-      //console.log("radio value: ", selectedValue);
-  }
-  
+    props.setRadioValue(selectedValue);
+    //console.log("radio value: ", selectedValue);
+  };
+
   return (
     <Container>
       <Wrapper>
         <Left>
           <SearchContainer>
             <SearchIcon style={{ color: "white" }} />
-            <Input value={props.searchValue}
-             onChange={(event) => props.setSearchValue(event.target.value)}
-             placeholder="Search" />
+            <Input
+              value={props.searchValue}
+              onChange={(event) => props.setSearchValue(event.target.value)}
+              placeholder="Search"
+            />
           </SearchContainer>
         </Left>
         <Center>
           <Title>Year</Title>
           <SliderWrapper>
-            <Value>{value[0]}</Value>
+            <Value>{props.yearValue[0]}</Value>
             <Slider
               getAriaLabel={() => "Year range"}
-              value={value}
+              value={props.yearValue}
               onChange={handleChange}
               valueLabelDisplay="auto"
               min={1950}
@@ -116,34 +115,39 @@ const Navbar = (props) => {
                 color: "#bdbdbd",
               }}
             />
-            <Value>{value[1]}</Value>
+            <Value>{props.yearValue[1]}</Value>
           </SliderWrapper>
         </Center>
         <Right>
           <Title>Type</Title>
           <RadioWrapper>
-              <FormControl>
-            <RadioGroup selectedValue={props.radioValue} onChange={handleRadioChange} row>
-              <FormControlLabel 
-              value="Any" 
-              control={<Radio />} 
-              label="Any" />
-              <FormControlLabel
-                value="movie"
-                control={<Radio />}
-                label="Movies"
-              />
-              <FormControlLabel
-                value="series"
-                control={<Radio />}
-                label="Series"
-              />
-              <FormControlLabel
-                value="episode"
-                control={<Radio />}
-                label="Episodes"
-              />
-            </RadioGroup>
+            <FormControl>
+              <RadioGroup
+                selectedValue={props.radioValue}
+                onChange={handleRadioChange}
+                row
+              >
+                <FormControlLabel 
+                  value="Any" 
+                  control={<Radio />} 
+                  label="Any" 
+                />
+                <FormControlLabel
+                  value="movie"
+                  control={<Radio />}
+                  label="Movies"
+                />
+                <FormControlLabel
+                  value="series"
+                  control={<Radio />}
+                  label="Series"
+                />
+                <FormControlLabel
+                  value="episode"
+                  control={<Radio />}
+                  label="Episodes"
+                />
+              </RadioGroup>
             </FormControl>
           </RadioWrapper>
         </Right>
